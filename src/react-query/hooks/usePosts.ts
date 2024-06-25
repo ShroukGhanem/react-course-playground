26badf627d2233dbe06194ff26f90987c4d8289e
Shrouk Ghanem <Shrouk.Ghanem@integrant.com>
2024-06-25 13:22:21 +0300

    Fetching posts with a custom hook

---
+++ src/react-query/hooks/usePosts.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+const usePosts = () => {
+  const fetchPsts = () =>
+    axios
+      .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.data);
+
+  return useQuery<Post[], Error>({
+    queryKey: ["posts"],
+    queryFn: fetchPsts,
+    staleTime: 120000,
+  });
+};
+
+export default usePosts;
